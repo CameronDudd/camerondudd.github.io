007eb252a78591de479ac0cfba7841414c9685f5
CameronDudd <110551761+CameronDudd@users.noreply.github.com>
2023-05-15 22:01:50 +0100

    modify to just show codewars badge

--- src/App.js
+++ src/App.js
@@ -5,19 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <img src="https://www.codewars.com/users/SigmaiMu/badges/large" alt="CodeWars SVG"/>
       </header>
+      <body className="App-body">
+      </body>
     </div>
   );
 }
