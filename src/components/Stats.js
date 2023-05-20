1b7068298749cd8233956efdbf3fec942dbeb8a0
CameronDudd <110551761+CameronDudd@users.noreply.github.com>
2023-05-20 13:43:23 +0100

    add Stats.js

---
+++ src/components/Stats.js
@@ -0,0 +1,19 @@
+export function TypeRacer() {
+    return (
+        <a href="https://data.typeracer.com/pit/profile?user=sigmaimu&ref=badge" target="_blank" rel="noreferrer">
+            <img id="typeracer" src="https://data.typeracer.com/misc/badge?user=sigmaimu"
+                 border="0"
+                 alt="TypeRacer.com scorecard for user sigmaimu"/>
+        </a>
+    );
+}
+
+export function CodeWars() {
+    return (
+        <a href="https://www.codewars.com/users/CameronDudd" target="_blank" rel="noreferrer">
+            <img id="codewars" src="https://www.codewars.com/users/CameronDudd/badges/large"
+                 border="0"
+                 alt="codewars.com scorecard for user CameronDudd"/>
+        </a>
+    );
+}
