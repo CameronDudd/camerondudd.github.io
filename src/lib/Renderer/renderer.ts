96f65a6007c93c9e82af1dc72988f40d4b43a46b
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-10-10 23:00:06 +0100

    feat(renderer): drawMesh3DOrthographic

--- src/lib/Renderer/renderer.ts
+++ src/lib/Renderer/renderer.ts
@@ -3,33 +3,35 @@ import type { Mesh3D } from 'lib/Geometry/geometry';
 const SQRT3 = Math.sqrt(3);
 const RECIPROCAL_SQRT6 = 1 / Math.sqrt(6);
 
-export function drawMesh3DIsometric(canvas: HTMLCanvasElement, mesh: Mesh3D) {
-  if (!canvas.getContext) return;
-
+export function drawMesh3DOrthographic(
+  canvas: HTMLCanvasElement,
+  mesh: Mesh3D
+) {
   const ctx = canvas.getContext('2d');
   if (!ctx) return;
-
   ctx.beginPath();
-
-  for (const edge of mesh.edges) {
-    const start = mesh.vertices[edge.start];
-    const end = mesh.vertices[edge.end];
-
-    const startx = start.x;
-    const starty = start.y;
-    const startz = start.z;
-    const endx = end.x;
-    const endy = end.y;
-    const endz = end.z;
-
-    const startX = RECIPROCAL_SQRT6 * (SQRT3 * startx - SQRT3 * startz);
-    const startY = RECIPROCAL_SQRT6 * (startx + 2 * starty + startz);
-    const endX = RECIPROCAL_SQRT6 * (SQRT3 * endx - SQRT3 * endz);
-    const endY = RECIPROCAL_SQRT6 * (endx + 2 * endy + endz);
-
-    ctx.moveTo(startX, startY);
-    ctx.lineTo(endX, endY);
+  for (const { start, end } of mesh.edges) {
+    const s = mesh.vertices[start];
+    const e = mesh.vertices[end];
+    ctx.moveTo(s.x, s.y);
+    ctx.lineTo(e.x, e.y);
   }
+  ctx.stroke();
+}
 
+export function drawMesh3DIsometric(canvas: HTMLCanvasElement, mesh: Mesh3D) {
+  const ctx = canvas.getContext('2d');
+  if (!ctx) return;
+  ctx.beginPath();
+  for (const { start, end } of mesh.edges) {
+    const s = mesh.vertices[start];
+    const e = mesh.vertices[end];
+    const sx = RECIPROCAL_SQRT6 * (SQRT3 * s.x - SQRT3 * s.z);
+    const sy = RECIPROCAL_SQRT6 * (s.x + 2 * s.y + s.z);
+    const ex = RECIPROCAL_SQRT6 * (SQRT3 * e.x - SQRT3 * e.z);
+    const ey = RECIPROCAL_SQRT6 * (e.x + 2 * e.y + e.z);
+    ctx.moveTo(sx, sy);
+    ctx.lineTo(ex, ey);
+  }
   ctx.stroke();
 }
