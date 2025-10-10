38805e4e011beac3867f56f435fdea437703d73d
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-10-10 12:56:39 +0100

    feat(camerondudd.github.io): initial commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'es5',
+  plugins: ['prettier-plugin-svelte'],
+  svelteSortOrder: 'options-scripts-markup-styles',
+  svelteStrictMode: true,
+  svelteBracketNewLine: true,
+  svelteAllowShorthand: true,
+};
