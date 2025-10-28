1c9ac6d3fd3fdbda04703c752a735605f9706f40
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-10-28 18:12:37 +0000

    ci: fix workflow config

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,32 @@
-import config from "eslint-config-xo";
 import { defineConfig } from "eslint/config";
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import svelteParser from "svelte-eslint-parser";
+import sveltePlugin from "eslint-plugin-svelte";
 
-export default defineConfig([config]);
+export default defineConfig([
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: { "@typescript-eslint": ts },
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn"],
+    },
+  },
+  {
+    files: ["**/*.svelte"],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: [".svelte"],
+      },
+    },
+    plugins: { svelte: sveltePlugin },
+    rules: {
+      "svelte/no-at-html-tags": "error",
+    },
+  },
+]);
